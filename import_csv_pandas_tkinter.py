14e48eef6c12a5fdf63a8ec6c73c3b6960b8e069
ntarkos <100260758+ntarkos@users.noreply.github.com>
2022-03-14 22:40:06 +0200

    Add files via upload

---
+++ import_csv_pandas_tkinter.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import requests
+import tkinter as tk
+import sys
+
+url = "https://raw.githubusercontent.com/Sandbird/covid19-Greece/master/cases.csv"
+
+c = pd.read_csv(url)
+
+print (c)
+
+
+root = tk.Tk()
+root.geometry('600x600')
+
+
+dframe = c
+
+print(' COVID 2019 DATA DASHBOARD')
+
+print(dframe)
